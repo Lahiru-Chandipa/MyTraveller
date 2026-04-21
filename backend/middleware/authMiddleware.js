0dabfddffe2d037d50a1be6136f5a9369b7393f7
Lahiru Chandipa <lahiruchandipa44@gmail.com>
2026-04-22 02:49:05 +0530

    modify middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,21 +1,26 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/userModel');
+import jwt from 'jsonwebtoken';
 
-const protect = async (req, res, next) => {
-  let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select('-password');
-      next();
-    } catch (error) {
-      res.status(401).json({ message: 'Not authorized, token failed' });
-    }
-  }
-  if (!token) {
-    res.status(401).json({ message: 'Not authorized, no token' });
+const protect = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "No token" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ message: "Invalid token" });
   }
 };
 
-module.exports = { protect };
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role))
+      return res.status(403).json({ message: "Forbidden" });
+
+    next();
+  };
+};
+
+export { protect, authorizeRoles };

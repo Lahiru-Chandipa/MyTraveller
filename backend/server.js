3ac460b92e75e5ba62309c6e754b77a5d67f3998
Lahiru Chandipa <lahiruchandipa44@gmail.com>
2026-04-22 02:43:34 +0530

    modify server and db connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-const app = require('./app');
-const dotenv = require('dotenv');
+import app from './app.js';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config();
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 

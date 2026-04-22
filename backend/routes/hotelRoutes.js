5cba6d52b891c10376d8322c6bc24d959d973833
Lahiru Chandipa <lahiruchandipa44@gmail.com>
2026-04-23 01:57:47 +0530

    refactor hotelRoutes.js to remove role authorization from hotel creation route

--- backend/routes/hotelRoutes.js
+++ backend/routes/hotelRoutes.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { createHotel, getHotels } from '../controllers/hotelController.js';
-import { protect, authorizeRoles } from '../middleware/authMiddleware.js';
+import { protect } from '../middleware/authMiddleware.js';
 import upload from '../utils/upload.js';
 
 const router = express.Router();
 
+// APPLY AS HOTEL
 router.post(
   '/',
   protect,
-  authorizeRoles('hotel'),
   upload.array('images', 5),
   createHotel
 );

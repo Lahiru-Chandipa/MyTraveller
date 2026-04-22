aa934a578b972ef1de996a351a7ff8fade7305cb
Lahiru Chandipa <lahiruchandipa44@gmail.com>
2026-04-23 01:57:32 +0530

    refactor adminRoutes.js to add approval routes for drivers and hotels

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { adminLogin, getDashboard, getAllUsers, getAllBookings } from "../controllers/adminController.js";
 import { protect, authorizeRoles } from "../middleware/authMiddleware.js";
+import { approveDriver, approveHotel } from "../controllers/adminController.js";
 
 const router = express.Router();
 
@@ -9,5 +10,8 @@ router.post("/login", adminLogin);
 router.get("/dashboard", protect, authorizeRoles("admin"), getDashboard);
 router.get("/users", protect, authorizeRoles("admin"), getAllUsers);
 router.get("/bookings", protect, authorizeRoles("admin"), getAllBookings);
+
+router.put("/drivers/:id/approve", protect, authorizeRoles("admin"), approveDriver);
+router.put("/hotels/:id/approve", protect, authorizeRoles("admin"), approveHotel);
 
 export default router;

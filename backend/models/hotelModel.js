5b69469fa1e7cc3ec70687cd07ff9b651ff27990
Lahiru Chandipa <lahiruchandipa44@gmail.com>
2026-04-23 01:56:55 +0530

    refactor hotelModel.js to enhance schema validation by adding required fields for user, name, location, contact, and pricePerNight

--- backend/models/hotelModel.js
+++ backend/models/hotelModel.js
@@ -5,16 +5,41 @@ const hotelSchema = new mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
+      required: true,
     },
 
-    name: String,
-    location: String,
-    description: String,
-    contact: String,
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    contact: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    pricePerNight: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
 
     images: [String],
 
     isApproved: {
+      type: Boolean,
+      default: false,
+    },
+    isAvailable: {
       type: Boolean,
       default: true,
     },

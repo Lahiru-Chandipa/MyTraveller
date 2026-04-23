09ed81bab602e1ca5681983cd0960ce0e2cd03a5
Lahiru Chandipa <lahiruchandipa44@gmail.com>
2026-04-23 23:56:44 +0530

    refactor userModel.js to add role field from user schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -23,7 +23,11 @@ const userSchema = new mongoose.Schema(
       trim: true,
     },
 
-    
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
 
     isApproved: {
       type: Boolean,

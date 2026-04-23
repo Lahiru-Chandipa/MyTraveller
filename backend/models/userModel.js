d559483493342ee9e3aaf3e3dfee7634bddbaab0
Lahiru Chandipa <lahiruchandipa44@gmail.com>
2026-04-23 23:54:25 +0530

    refactor userModel.js to remove role field from user schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -23,11 +23,7 @@ const userSchema = new mongoose.Schema(
       trim: true,
     },
 
-    role: {
-      type: String,
-      enum: ["user", "admin"],
-      default: "user",
-    },
+    
 
     isApproved: {
       type: Boolean,
